0ed5ce9d5d62bae631cc0c40970bd53537d203ad
masalib <masalib@gmail.com>
2020-11-22 14:02:15 +0900

    material-uiのSignup画面を作成とFirebaseのAPIキーを表示していた部分を削除

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
+import Signup from "./Signup"
+
 function App() {
   return (
     <>
     <div>Hello,World </div>
-    <div>{`REACT_APP_APIKEY:${process.env.REACT_APP_APIKEY}`}</div>
-    <div>{`REACT_APP_AUTHDOMAIN:${process.env.REACT_APP_AUTHDOMAIN}`}</div>
-    <div>{`REACT_APP_DATABASEURL:${process.env.REACT_APP_DATABASEURL}`}</div>
-    <div>{`REACT_APP_PROJECTID:${process.env.REACT_APP_PROJECTID}`}</div>
-    <div>{`REACT_APP_STORAGEBUCKET:${process.env.REACT_APP_STORAGEBUCKET}`}</div>
-    <div>{`REACT_APP_MESSAGINGSENDERID:${process.env.REACT_APP_MESSAGINGSENDERID}`}</div>
-    <div>{`REACT_APP_APPID:${process.env.REACT_APP_APPID}`}</div>
-    <div>{`REACT_APP_MEASUREMENTID:${process.env.REACT_APP_MEASUREMENTID}`}</div>
-
+    <Signup />
     </>
   );
 }
