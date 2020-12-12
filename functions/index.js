e6b6bc2d67633a8b96a197fdc85a17c0b5b1573a
masalib <masalib@gmail.com>
2020-12-12 18:25:44 +0900

    emulatorの設定追加 Cloud Functions：ユーザー作成時、FireStoreにユーザー情報をinsertする処理を追加

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,39 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello World logs!", {structuredData: true});
     response.send("Hello World");
 })
+
+exports.helloWorld2 = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello2 World logs!", {structuredData: true});
+    functions.logger.info("emulator test2");
+    response.send("Hello World2");
+})
+
+exports.onCreateUser = functions.auth.user().onCreate(async(userRecord, _context) => {
+
+    var email = userRecord.email ? userRecord.email : ""
+    var photoURL = userRecord.photoURL ? userRecord.photoURL : ""
+    var displayName = userRecord.displayName ? userRecord.displayName : ""
+
+    var serverTimestamp = admin.firestore.FieldValue.serverTimestamp()
+
+    functions.logger.info("user created", userRecord);
+    await admin.firestore().collection('members').doc(userRecord.uid).set({
+        docId: userRecord.uid,
+        uid: userRecord.uid,
+        email: email,
+        displayName: displayName,
+        photoURL: photoURL,
+        departmentId: "9999",
+        createdAt: serverTimestamp,
+        updatedAt: serverTimestamp,
+    });
+    return;
+});
+
+
+
