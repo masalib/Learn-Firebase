3172b8fbaee658be70ea38fcea9c4dbb3a9b6e9e
masalib <masalib@gmail.com>
2020-12-15 01:01:58 +0900

    Cloud Functionsでslackに通知する Cloud Functionsでメール送信(SendGrid)

--- functions/index.js
+++ functions/index.js
@@ -35,5 +35,62 @@ exports.onCreateUser = functions.auth.user().onCreate(async(userRecord, _context
     return;
 });
 
+//mail送信API
+const sendgrid  = require('@sendgrid/mail')
+exports.sendMail = functions.https.onRequest(async (request, response) => {
+    functions.logger.info("sendMail start");
 
+    const apiKey = functions.config().sendgrid_service.key;
+    const fromEmail = functions.config().sendgrid_service.email;
+    const fromName = functions.config().sendgrid_service.name;
 
+    sendgrid.setApiKey(apiKey);
+    const msg = {
+        to: "m-hirano@mediaseek.co.jp",
+        from: `${fromName} <${fromEmail}>`,
+        subject: `【${fromName}】問い合わせ受理メール`,
+        text: `XXXX様
+問い合わせありがとうございます。担当者が確認して返信したいと思いますので
+少々お待ち下さい。
+
+このメールには返信できません。`
+    };
+
+    sendgrid.send(msg).then((result )=>{
+        functions.logger.info("sendMail success");
+        response.send("sendMail success");
+        return console.log("Successfully sent message:", result);
+    })
+    .catch((error)=>{
+        functions.logger.error("sendMail error");
+        functions.logger.error(error);
+        response.send("sendMail error");
+        return console.log("Error sending message:", error);
+    })
+    //return;
+})
+//webhook-url-in-slack
+const { IncomingWebhook } = require('@slack/webhook');
+exports.webhookslack = functions.https.onRequest(async (request, response) => {
+    functions.logger.info("webhookslack start");
+    const webhook = new IncomingWebhook(functions.config().slack_service.adminurl);
+    const testId = 'test0001';
+    const postId = 'post0001';
+    const commentName = 'masalib';
+    const array = [
+        `新しいコメントが届きました！`,
+        `<https://xxxxx.jp/jump?id=${testId}&post=${postId}|こちらから飛ぶ>`,
+        `名前: ${commentName}`
+    ]
+
+    // slack APIが受け取れるオブジェクトを作成する
+    const data = {
+        text: array.join('\n'), // 配列を`\n`で結合、改行する
+        username: 'FirebaseApp-BOT',
+        icon_emoji: ':ghost:'
+    }
+
+    await webhook.send(data);
+    response.send("webhookslack success");
+    return;
+})
